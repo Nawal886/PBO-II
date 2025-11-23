f7fbbc4a39efcdc448998dfa9c152ba86d705b64
Nawal Haromaen <nawalharomaen@gmail.com>
2025-11-24 03:47:45 +0700

    Menambahkan file baru minggu ini

---
+++ Pertemuan 06/Tugas/P6_4_714240038/P6_4_714240038/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace P6_4_714240038
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.txtNoHP = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtKodePromo = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtKonfirmasi = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnDaftar = new System.Windows.Forms.Button();
+            this.epError = new System.Windows.Forms.ErrorProvider(this.components);
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.epError)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtNama
+            // 
+            this.txtNama.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtNama.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNama.Location = new System.Drawing.Point(253, 56);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(208, 25);
+            this.txtNama.TabIndex = 0;
+            this.txtNama.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNama_KeyPress_1);
+            // 
+            // txtNoHP
+            // 
+            this.txtNoHP.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtNoHP.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNoHP.Location = new System.Drawing.Point(253, 105);
+            this.txtNoHP.Name = "txtNoHP";
+            this.txtNoHP.Size = new System.Drawing.Size(208, 25);
+            this.txtNoHP.TabIndex = 1;
+            this.txtNoHP.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNoHP_KeyPress);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtEmail.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(253, 159);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(208, 25);
+            this.txtEmail.TabIndex = 2;
+            this.txtEmail.Leave += new System.EventHandler(this.txtEmail_Leave);
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtUsername.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsername.Location = new System.Drawing.Point(253, 211);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(208, 25);
+            this.txtUsername.TabIndex = 3;
+            // 
+            // txtKodePromo
+            // 
+            this.txtKodePromo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtKodePromo.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKodePromo.Location = new System.Drawing.Point(253, 264);
+            this.txtKodePromo.Name = "txtKodePromo";
+            this.txtKodePromo.Size = new System.Drawing.Size(208, 25);
+            this.txtKodePromo.TabIndex = 4;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPassword.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(253, 317);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(208, 25);
+            this.txtPassword.TabIndex = 5;
+            // 
+            // txtKonfirmasi
+            // 
+            this.txtKonfirmasi.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtKonfirmasi.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKonfirmasi.Location = new System.Drawing.Point(253, 361);
+            this.txtKonfirmasi.Name = "txtKonfirmasi";
+            this.txtKonfirmasi.Size = new System.Drawing.Size(208, 25);
+            this.txtKonfirmasi.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Gold;
+            this.label1.Location = new System.Drawing.Point(44, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Nama Lengkap";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Gold;
+            this.label2.Location = new System.Drawing.Point(44, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Nomor HP";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Gold;
+            this.label3.Location = new System.Drawing.Point(45, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Email";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Gold;
+            this.label4.Location = new System.Drawing.Point(44, 219);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Username";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Gold;
+            this.label5.Location = new System.Drawing.Point(44, 325);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Password";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Gold;
+            this.label6.Location = new System.Drawing.Point(45, 272);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 17);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Kode Promo";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Gold;
+            this.label7.Location = new System.Drawing.Point(44, 369);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(130, 17);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Konfirmasi Password";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(431, 267);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 13);
+            this.label8.TabIndex = 14;
+            // 
+            // btnDaftar
+            // 
+            this.btnDaftar.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDaftar.Location = new System.Drawing.Point(167, 415);
+            this.btnDaftar.Name = "btnDaftar";
+            this.btnDaftar.Size = new System.Drawing.Size(191, 23);
+            this.btnDaftar.TabIndex = 15;
+            this.btnDaftar.Text = "DAFTAR SEKARANG";
+            this.btnDaftar.UseCompatibleTextRendering = true;
+            this.btnDaftar.UseVisualStyleBackColor = true;
+            this.btnDaftar.Click += new System.EventHandler(this.btnDaftar_Click);
+            // 
+            // epError
+            // 
+            this.epError.ContainerControl = this;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(142, 9);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(225, 25);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "DAFTAR MEMBER BARU";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(572, 450);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.btnDaftar);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtKonfirmasi);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtKodePromo);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtNoHP);
+            this.Controls.Add(this.txtNama);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Cinema Member Registration";
+            ((System.ComponentModel.ISupportInitialize)(this.epError)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.TextBox txtNoHP;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtKodePromo;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtKonfirmasi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnDaftar;
+        private System.Windows.Forms.ErrorProvider epError;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Pertemuan 06/Tugas/P6_4_714240038/P6_4_714240038/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P6_4_714240038
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Mengatur Upper/Lower case langsung dari properties
+            // Agar Textbox otomatis mengubah input user
+            txtUsername.CharacterCasing = CharacterCasing.Lower;
+            txtKodePromo.CharacterCasing = CharacterCasing.Upper;
+        }
+        private void btnDaftar_Click(object sender, EventArgs e)
+        {
+            // Logika: Cek Validasi dulu -> Jika Benar -> Tampilkan MessageBox
+            if (ValidasiForm())
+            {
+                // Menyusun string agar tampilannya rapi seperti di Screenshot P5_4
+                string infoPendaftaran =
+                    "Nama : " + txtNama.Text + "\n" +
+                    "Username : " + txtUsername.Text + "\n" +
+                    "No HP : " + txtNoHP.Text + "\n" +
+                    "Email : " + txtEmail.Text + "\n" +
+                    "Kode Promo : " + txtKodePromo.Text + "\n" +
+                    "Status : Validasi Berhasil";
+
+                // Menampilkan MessageBox dengan Ikon Informasi (biru)
+                MessageBox.Show(
+                    infoPendaftaran,             // Isi pesan
+                    "Informasi Pendaftaran",     // Judul (Header)
+                    MessageBoxButtons.OK,        // Tombol
+                    MessageBoxIcon.Information   // Ikon 'i' biru
+                );
+            }
+            else
+            {
+                // Jika ada validasi yang gagal, munculkan peringatan error
+                MessageBox.Show(
+                    "Masih ada data yang belum sesuai. Silakan cek tanda seru merah!",
+                    "Validasi Gagal",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning
+                );
+            }
+        }
+
+        private void txtNoHP_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+                epError.SetError(txtNoHP, "Hanya boleh angka!");
+            }
+            else
+            {
+                epError.SetError(txtNoHP, "");
+            }
+        }
+
+       
+
+        private void txtEmail_Leave(object sender, EventArgs e)
+        {
+            string emailPattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+            if (!string.IsNullOrEmpty(txtEmail.Text) && !Regex.IsMatch(txtEmail.Text, emailPattern))
+            {
+                epError.SetError(txtEmail, "Format email salah!");
+            }
+        }
+
+        private void txtNama_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsLetter(e.KeyChar) && !char.IsWhiteSpace(e.KeyChar))
+            {
+                e.Handled = true;
+                epError.SetError(txtNama, "Hanya boleh huruf!");
+            }
+            else
+            {
+                epError.SetError(txtNama, "");
+            }
+        }
+
+        // Fungsi Pengecekan Menyeluruh
+        private bool ValidasiForm()
+        {
+            bool isValid = true;
+            epError.Clear(); // Bersihkan error lama
+
+            // 1. Cek Nama (Required) 
+            if (string.IsNullOrWhiteSpace(txtNama.Text))
+            {
+                epError.SetError(txtNama, "Nama wajib diisi!");
+                isValid = false;
+            }
+
+            // 2. Cek No HP (Required)
+            if (string.IsNullOrWhiteSpace(txtNoHP.Text))
+            {
+                epError.SetError(txtNoHP, "Nomor HP wajib diisi!");
+                isValid = false;
+            }
+
+            // 3. Cek Email (Regex Validator) 
+            string emailPattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+            if (string.IsNullOrWhiteSpace(txtEmail.Text))
+            {
+                epError.SetError(txtEmail, "Email wajib diisi!");
+                isValid = false;
+            }
+            else if (!Regex.IsMatch(txtEmail.Text, emailPattern))
+            {
+                epError.SetError(txtEmail, "Format email salah! (Harus ada @ dan .)");
+                isValid = false;
+            }
+
+            // 4. Cek Username (Required + Lowercase otomatis)
+            if (string.IsNullOrWhiteSpace(txtUsername.Text))
+            {
+                epError.SetError(txtUsername, "Username wajib diisi!");
+                isValid = false;
+            }
+
+            // 5. Cek Password (Length Validator) 
+            if (string.IsNullOrWhiteSpace(txtPassword.Text))
+            {
+                epError.SetError(txtPassword, "Password wajib diisi!");
+                isValid = false;
+            }
+            else if (txtPassword.Text.Length < 8)
+            {
+                epError.SetError(txtPassword, "Password minimal 8 karakter!");
+                isValid = false;
+            }
+
+            // 6. Cek Konfirmasi Password (Comparison) 
+            if (txtKonfirmasi.Text != txtPassword.Text)
+            {
+                epError.SetError(txtKonfirmasi, "Password tidak sama!");
+                isValid = false;
+            }
+
+            return isValid; // Kembalikan status (True jika semua aman, False jika ada error)
+        }
+    }
+    }
+    
+    
+    
+
