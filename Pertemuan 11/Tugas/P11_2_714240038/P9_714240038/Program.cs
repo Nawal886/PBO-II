a585daad24361c4fcfd7d15a3b8982dcb88b7593
nawal886 <nawalharomaen@gmail.com>
2025-12-28 22:55:13 +0700

    menambahkan materi tugas dan praktikum 11

---
+++ Pertemuan 11/Tugas/P11_2_714240038/P9_714240038/Program.cs
@@ -0,0 +1,23 @@
+﻿using P9_714240038.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P9_714240038
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new ParentForm());
+        }
+    }
+}
