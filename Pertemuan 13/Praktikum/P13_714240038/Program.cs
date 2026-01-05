395b23f7db0bfe36c91c85b56530f65e8bc75d31
nawal886 <nawalharomaen@gmail.com>
2026-01-05 22:47:38 +0700

    upload pertemuan ke 13

---
+++ Pertemuan 13/Praktikum/P13_714240038/Program.cs
@@ -0,0 +1,28 @@
+﻿using OfficeOpenXml;
+using P9_714240061.view;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P9_714240061
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // Wajib untuk EPPlus v5+ (kalau untuk non commercial)
+            ExcelPackage.License.SetNonCommercialPersonal("ULBI");
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new StartUp());
+            Application.Run(new FormLogin());
+        }
+    }
+}
