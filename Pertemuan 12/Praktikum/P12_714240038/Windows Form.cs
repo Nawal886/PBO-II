26b5af3c1122f41e690b7dc47b25f5eebb4b300b
nawal886 <nawalharomaen@gmail.com>
2026-01-05 10:35:35 +0700

    s

---
+++ Pertemuan 12/Praktikum/P12_714240038/Windows Form.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P9_714240061
+{
+    public partial class Windows_Form : Form
+    {
+        public Windows_Form()
+        {
+            InitializeComponent();
+        }
+    }
+}
